1cf0abcbda31a69b110819f7e2b88a39d68b90a3
Ian <ssianfilipe@gmail.com>
2022-08-25 19:12:48 -0300

    add test of form component

---
+++ src/components/Form.test.tsx
@@ -0,0 +1,17 @@
+import { render, screen } from '@testing-library/react'
+import React from 'react'
+import Form from './Form'
+
+test("When the input is empty, new participants can't be added", () => {
+  render(<Form />)
+  // find the input on DOM
+  const input = screen.getByPlaceholderText(
+    'Fill with the names of the participants'
+  )
+  // find the button
+  const button = screen.getByRole('button')
+  // ensure the input is in the document
+  expect(input).toBeInTheDocument()
+  // ensure the button is disabled
+  expect(button).toBeDisabled()
+})
