b5f5fb4109fbb7264289278260143b08c7acc503
Ian <ssianfilipe@gmail.com>
2022-08-25 19:12:55 -0300

    add form component

---
+++ src/components/Form.tsx
@@ -0,0 +1,13 @@
+function Form() {
+  return (
+    <form>
+      <input
+        type="text"
+        placeholder="Fill with the names of the participants"
+      ></input>
+      <button disabled>Add</button>
+    </form>
+  )
+}
+
+export default Form
